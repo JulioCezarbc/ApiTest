4b5a1f12d815aaf2e6075bc03a6b942ccf191a47
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-05 14:41:52 -0300

    refactor: UserController findById

--- src/main/java/br/com/julio/ApiTest/controller/UserController.java
+++ src/main/java/br/com/julio/ApiTest/controller/UserController.java
@@ -1,6 +1,8 @@
 package br.com.julio.ApiTest.controller;
 
 import br.com.julio.ApiTest.domain.User;
+import br.com.julio.ApiTest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(new User(1L,"julio","julio@gmail.com","123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 
