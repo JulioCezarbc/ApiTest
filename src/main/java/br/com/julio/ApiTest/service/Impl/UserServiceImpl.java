31e91add38aa30fba28b3fd58ed8aa8afa05d0f2
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-05 14:41:36 -0300

    feat: Interface UserServiceImpl, findById

---
+++ src/main/java/br/com/julio/ApiTest/service/Impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.julio.ApiTest.service.Impl;
+
+import br.com.julio.ApiTest.domain.User;
+import br.com.julio.ApiTest.repository.UserRepository;
+import br.com.julio.ApiTest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
