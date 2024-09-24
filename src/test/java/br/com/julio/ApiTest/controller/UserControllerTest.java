6ea0aff988b9823e9af935a32c2f991d697add29
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-24 14:23:48 -0300

    feat: Test para endpoint FindById implementado

--- src/test/java/br/com/julio/ApiTest/controller/UserControllerTest.java
+++ src/test/java/br/com/julio/ApiTest/controller/UserControllerTest.java
@@ -4,13 +4,18 @@ import br.com.julio.ApiTest.domain.DTO.UserDTO;
 import br.com.julio.ApiTest.domain.User;
 import br.com.julio.ApiTest.service.Impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserControllerTest {
@@ -36,6 +41,22 @@ class UserControllerTest {
         startUser();
     }
 
+    @Test
+    void whenFindByIdThenReturnSuccess(){
+        when(service.findById(anyLong())).thenReturn(user);
+        when(mapper.map(any(),any())).thenReturn(userData);
+
+        ResponseEntity<UserDTO> response = controller.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class,response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+        assertEquals(ID,response.getBody().getId());
+        assertEquals(NAME,response.getBody().getName());
+        assertEquals(MAIL,response.getBody().getEmail());
+        assertEquals(PASSWORD,response.getBody().getPassword());
+    }
 
     private void startUser(){
         user = new User(ID, NAME, MAIL, PASSWORD);
