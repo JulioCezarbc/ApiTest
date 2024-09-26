c1e4d7d70af44af87019376bcd1bcb07e655fa44
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-26 11:43:28 -0300

    feat: Test para o ControllerExceptionHandler, metodo DataIntegrityViolation implementado

--- src/test/java/br/com/julio/ApiTest/controller/exception/ControllerExceptionHandlerTest.java
+++ src/test/java/br/com/julio/ApiTest/controller/exception/ControllerExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.julio.ApiTest.controller.exception;
 
+import br.com.julio.ApiTest.service.exception.DataIntegrityViolationException;
 import br.com.julio.ApiTest.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import javax.print.attribute.DateTimeSyntax;
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -23,8 +18,6 @@ class ControllerExceptionHandlerTest {
 
     @InjectMocks
     private ControllerExceptionHandler exceptionHandler;
-
-
 
     @BeforeEach
     void setUp(){
@@ -45,5 +38,21 @@ class ControllerExceptionHandlerTest {
         assertEquals("Object not found", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
     }
+
+    @Test
+    void dataIntegrityViolationException(){
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(new DataIntegrityViolationException("Email already used")
+                , new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Email already used", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+    }
+
 
 }
